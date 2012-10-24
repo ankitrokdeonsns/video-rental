f0e9c9bf42eb63d0ad66315497fe0c7e84046bb2
wikipedia <ankitrokdeonsns@gmail.com>
2012-10-24 21:40:08 +0530

    (ankit) extracted methods to make statement() smaller

--- src/com/twu/refactor/Customer.java
+++ src/com/twu/refactor/Customer.java
@@ -26,37 +26,11 @@ public class Customer {
 		Iterator<Rental> rentals = rentalList.iterator();
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasNext()) {
-			double thisAmount = 0;
-			Rental each = rentals.next();
-
-			// determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-
-			}
-
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
-				frequentRenterPoints++;
-
-			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
+            Rental each = rentals.next();
+            double thisAmount = calculateAmoutFor(each);
+            frequentRenterPoints = calculateFrequentRenterPointsFor(frequentRenterPoints, each);
+            // show figures for this rental
+			result += statementForEachRental(each, thisAmount);
 			totalAmount += thisAmount;
 
 		}
@@ -66,5 +40,43 @@ public class Customer {
 				+ " frequent renter points";
 		return result;
 	}
+
+    private String statementForEachRental(Rental rental, double thisAmount) {
+        return "\t" + rental.getMovie().getTitle() + "\t"
+                + String.valueOf(thisAmount) + "\n";
+    }
+
+    private int calculateFrequentRenterPointsFor(int frequentRenterPoints, Rental rental) {
+        // add frequent renter points
+        frequentRenterPoints++;
+        // add bonus for a two day new release rental
+        if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+                && rental.getDaysRented() > 1)
+            frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
+
+    private double calculateAmoutFor(Rental rental) {
+        double thisAmount = 0;
+
+        // determine amounts for each line
+        switch (rental.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+            thisAmount += 2;
+            if (rental.getDaysRented() > 2)
+                thisAmount += (rental.getDaysRented() - 2) * 1.5;
+            break;
+        case Movie.NEW_RELEASE:
+            thisAmount += rental.getDaysRented() * 3;
+            break;
+        case Movie.CHILDRENS:
+            thisAmount += 1.5;
+            if (rental.getDaysRented() > 3)
+                thisAmount += (rental.getDaysRented() - 3) * 1.5;
+            break;
+
+        }
+        return thisAmount;
+    }
 
 }
